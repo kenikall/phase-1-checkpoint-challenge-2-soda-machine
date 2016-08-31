cbf8c7d7b0d32de9fcda3f6dca97b06271f61f66
K. Mannah Kallon <kenikall@gmail.com>
2016-08-30 21:05:06 -0700

    I don't know why I had so much trouble with this yesterday.

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,20 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.each{|can| return can if can.brand == soda_brand}
+    return nil
   end
 
   def sell(soda_brand)
+    can = find_soda(soda_brand)
+    return nil if can == nil
+    @sodas.delete_at(@sodas.index(can))
+    @cash += can.price
+    return can
   end
 
 end
